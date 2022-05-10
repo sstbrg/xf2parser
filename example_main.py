1221f75b9d6d0b40a42126c7eef66ce0688c4141
Stas Steinberg <sstbrg@gmail.com>
2022-05-10 08:05:21 +0300

    added s3 interface

--- example_main.py
+++ example_main.py
@@ -4,7 +4,15 @@ from xtrbtparser.EDFExport import *
 from botocore.config import Config
 import boto3
 from urllib.parse import urlparse
+from pathlib import Path
 import os
+
+
+def get_matching_s3_objects(bucket, prefix=''):
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket(bucket)
+    return [object_summary.key for object_summary in my_bucket.objects.filter(Prefix=prefix)]
+
 
 clientconfig = Config(
    retries = {
@@ -14,38 +22,31 @@ clientconfig = Config(
 )
 client = boto3.client('s3', config=clientconfig)
 
-def get_matching_s3_objects(bucket, prefix=''):
-    s3 = boto3.resource('s3')
-    my_bucket = s3.Bucket(bucket)
-    return [object_summary.key for object_summary in my_bucket.objects.filter(Prefix=prefix)]
 
 input = urlparse(os.environ.get('INPUT',
                                  's3://xtrodes-datasets/public/cognito/xtrodesclient/us-east-1:092b1e60-489d-44d3-99ee-92546c2fb72f/20220503_sine31hz_1mv_xf2-.~1652126233/RECORDS/'))
-output = urlparse(os.environ.get('OUTPUT', 's3://x-cognito/xf2parser/asaf_fw_sine_31_1'))
+output = urlparse(os.environ.get('OUTPUT', 's3://x-cognito/xf2parser/20220503_sine31hz_1mv_xf2-.~1652126233'))
 
-# # config = {'window_size_in_samples': int(os.environ.get('WINDOW_SIZE_IN_SAMPLES', "8000")),
-# #           'power_threshold': float(os.environ.get('POWER_THRESHOLD', "0.000700")),
-# #           'correlation_threshold': float(os.environ.get('CORRELATION_THRESHOLD', "0.75")),
-# #           'low_cutoff_frequency': int(os.environ.get('LOW_CUTOFF_FREQUENCY', "1000")),
-# #           'high_cutoff_frequency': int(os.environ.get('HIGH_CUTOFF_FREQUENCY', "1500")),
-# #           'filter_order': int(os.environ.get('FILTER_ORDER', "16")),
-# #           'stride_size_in_samples': int(os.environ.get('STRIDE_SIZE_IN_SAMPLES', "100"))
-# #           }
-#
-#
+local_work_directory = 'data'
+local_output_path = os.path.join('result', 'result.edf')
+
 # download data
 files = natsort.natsorted(get_matching_s3_objects(bucket=input.netloc, prefix=input.path[1:]))
+
 for f in files:
-    client.download_file(input.netloc, f, local_file_path)
-print('INFO: done pulling file')
+    local_file_path = os.path.join(local_work_directory, Path(f).name)
+    if not os.path.isfile(local_file_path):
+        print('INFO: downloading %s' % f)
+        client.download_file(input.netloc, f, local_file_path)
+print('INFO: done pulling files')
 
 # f = File(filepath=local_file_path)
-parser = Parser(work_directory = r'C:\Users\Stas\Documents\xtrodes\data\20220503_sine31hz_1mv_xf2\RECORDS')
+parser = Parser(work_directory = local_work_directory)
 parser.process_files()
-edfer = EDFProcessor(file_path=r'C:\Users\Stas\Documents\xtrodes\data\20220503_sine31hz_1mv_xf2\adc.edf')
+edfer = EDFProcessor(file_path=local_output_path)
 edfer.dump_to_edf(data_in=np.transpose(parser.data[REC_TYPE_ADC]), sample_rate=4000)
 
-# f = File(filepath='C:\\Users\\Stas\\Documents\\xtrodes\\data\\20220503_sine31hz_1mv_xf2\\RECORDS\\TEST13.XF2')
-# f.get_records()
-# f.read_data()
-# print(1)
+output_prefix = output.path[1:]
+print('INFO: uploading %s' % output_prefix)
+client.upload_file(local_output_path, 'x-cognito', output_prefix)
+print('INFO: finished uploading')
